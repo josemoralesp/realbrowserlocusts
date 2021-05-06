c9f22229b50d813f2c9bbefd46b1550fb12d7206
Jose Morales <jose.morales@Bens-MacBook-Pro.local>
2021-05-06 01:21:45 -0500

    [IMP] Set the right option to run selenium within a container

--- realbrowserlocusts/locusts.py
+++ realbrowserlocusts/locusts.py
@@ -60,15 +60,14 @@ class HeadlessChromeLocust(RealBrowserLocust):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         options = webdriver.ChromeOptions()
+        options.add_argument('headless')
         options.add_argument('--no-sandbox')
-        options.add_argument('--ignore-certificate-errors')
-        options.add_argument('--headless')
+        options.add_argument('--disable-gpu')
         options.add_argument('--disable-dev-shm-usage')
         # options.add_argument('--remote-debugging-port=9222')
         options.add_argument('window-size={}x{}'.format(
             self.screen_width, self.screen_height
         ))
-        options.add_argument('disable-gpu')
         if self.proxy_server:
             _LOGGER.info('Using proxy: ' + self.proxy_server)
             options.add_argument('proxy-server={}'.format(self.proxy_server))
